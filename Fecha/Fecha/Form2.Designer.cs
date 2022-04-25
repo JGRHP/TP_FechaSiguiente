34635848cc859bf92c89cf38dde6bc069eb67233
José Rosas <89753064+JGRHP@users.noreply.github.com>
2022-04-24 21:21:16 -0300

    Add files via upload

---
+++ Fecha/Fecha/Form2.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Fecha
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDia = new System.Windows.Forms.Label();
+            this.lblMes = new System.Windows.Forms.Label();
+            this.lblAnio = new System.Windows.Forms.Label();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.lblLeyenda = new System.Windows.Forms.Label();
+            this.btnVerFecha = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDia
+            // 
+            this.lblDia.AutoSize = true;
+            this.lblDia.Location = new System.Drawing.Point(53, 18);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(24, 15);
+            this.lblDia.TabIndex = 0;
+            this.lblDia.Text = "Día";
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Location = new System.Drawing.Point(168, 18);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(29, 15);
+            this.lblMes.TabIndex = 1;
+            this.lblMes.Text = "Mes";
+            // 
+            // lblAnio
+            // 
+            this.lblAnio.AutoSize = true;
+            this.lblAnio.Location = new System.Drawing.Point(296, 18);
+            this.lblAnio.Name = "lblAnio";
+            this.lblAnio.Size = new System.Drawing.Size(29, 15);
+            this.lblAnio.TabIndex = 2;
+            this.lblAnio.Text = "Año";
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Location = new System.Drawing.Point(280, 47);
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(64, 23);
+            this.txtAnio.TabIndex = 3;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(152, 47);
+            this.txtMes.MaxLength = 2;
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(64, 23);
+            this.txtMes.TabIndex = 4;
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(34, 47);
+            this.txtDia.MaxLength = 2;
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(64, 23);
+            this.txtDia.TabIndex = 5;
+            //this.txtDia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDia_KeyPress);
+            //this.txtDia.Validated += new System.EventHandler(this.btnVerFecha_Click);
+            // 
+            // lblLeyenda
+            // 
+            this.lblLeyenda.AutoSize = true;
+            this.lblLeyenda.Location = new System.Drawing.Point(80, 148);
+            this.lblLeyenda.Name = "lblLeyenda";
+            this.lblLeyenda.Size = new System.Drawing.Size(216, 15);
+            this.lblLeyenda.TabIndex = 6;
+            this.lblLeyenda.Text = "Ingrese una fecha en formato numérico";
+            // 
+            // btnVerFecha
+            // 
+            this.btnVerFecha.Location = new System.Drawing.Point(138, 115);
+            this.btnVerFecha.Name = "btnVerFecha";
+            this.btnVerFecha.Size = new System.Drawing.Size(97, 20);
+            this.btnVerFecha.TabIndex = 7;
+            this.btnVerFecha.Text = "VerFecha";
+            this.btnVerFecha.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnVerFecha.UseVisualStyleBackColor = true;
+            this.btnVerFecha.Click += new System.EventHandler(this.btnVerFecha_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(391, 172);
+            this.Controls.Add(this.btnVerFecha);
+            this.Controls.Add(this.lblLeyenda);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtAnio);
+            this.Controls.Add(this.lblAnio);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.lblDia);
+            this.Name = "Form2";
+            this.Text = "Fecha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDia;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label lblAnio;
+        private System.Windows.Forms.TextBox txtAnio;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Label lblLeyenda;
+        private System.Windows.Forms.Button btnVerFecha;
+    }
+}
---
+++ Fecha/Fecha/Form2.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fecha
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+           
+            InitializeComponent();
+        }
+       
+        private void btnVerFecha_Click(object sender, EventArgs e)
+        {
+            int Dia = 0;
+            int Mes = 0;
+            int Anio = 0;
+            string DiaFecha;
+            string AnioFecha;
+            string MesFecha;
+
+            if (txtDia.Text.Any(x => !char.IsNumber(x)) || txtMes.Text.Any(x => !char.IsNumber(x)) || txtAnio.Text.Any(x => !char.IsNumber(x)))
+            {
+                MessageBox.Show("El formato de fecha debe ser numérico");
+                return;
+            }
+
+            Dia = int.Parse(txtDia.Text);
+            Mes = int.Parse(txtMes.Text);
+            Anio = int.Parse(txtAnio.Text);
+            Dia = Dia + 1;
+           
+            MesFecha = Convert.ToString(Mes);
+            DiaFecha = Convert.ToString(Dia);
+            AnioFecha = Convert.ToString(Anio);
+
+            switch (Mes)
+            {
+                case 1:
+                    if (Mes == 1)
+                    {                        
+                        MesFecha = "Enero";
+                        if(Dia > 0 && Dia < 31)
+                        {                           
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 31 && Dia <= 32)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Febrero";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }else if (Dia >= 32)
+                        {
+                            MessageBox.Show("Error");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                    }
+                    break;
+                case 2:
+                    if (Mes == 2)
+                    {
+                        MesFecha = "Febrero";
+                        if (Anio % 4 == 0 && Anio % 100 != 0 || Anio % 400 == 0)
+                        {
+                            if (Dia > 0 && Dia < 28)
+                            {
+                                MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+
+                            }
+                            else if (Dia >= 28 && Dia <= 29)
+                            {
+                                Dia = 1;
+                                DiaFecha = DiaFecha = Convert.ToString(Dia);
+                                MesFecha = "Marzo";
+                                MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                            }
+                        }
+                        else
+                        {
+                            if (Dia > 0 && Dia < 29)
+                            {
+                                MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                            }
+                            else if (Dia >= 29 && Dia <= 30)
+                            {
+                                Dia = 1;
+                                DiaFecha = DiaFecha = Convert.ToString(Dia);
+                                MesFecha = "Marzo";
+                                MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                            }
+                        }
+
+                    }
+                    break;
+
+                case 3:
+                    if (Mes == 3)
+                    {
+                        MesFecha = "Marzo";
+                        if (Dia > 0 && Dia < 31)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 31 && Dia <= 32)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Abril";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+
+                case 4:
+                    if (Mes == 4)
+                    {
+                        MesFecha = "Abril";
+                        if (Dia > 0 && Dia < 30)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 30 && Dia <= 31)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Mayo";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+
+                case 5:
+                    if (Mes == 5)
+                    {
+                        MesFecha = "Mayo";
+                        if (Dia > 0 && Dia < 31)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 31 && Dia <= 32)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Junio";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+
+                case 6:
+                    if (Mes == 6)
+                    {
+                        MesFecha = "Junio";
+                        if (Dia > 0 && Dia < 30)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 30 && Dia <= 31)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Julio";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+
+                case 7:
+                    if (Mes == 7)
+                    {
+                        MesFecha = "Julio";
+                        if (Dia > 0 && Dia < 31)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 31 && Dia <= 32)
+                        {                           
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Agosto";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+
+                case 8:
+                    if (Mes == 8)
+                    {
+                        MesFecha = "Agosto";
+                        if (Dia > 0 && Dia < 31)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 31 && Dia <= 32)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Septiembre";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+
+                case 9:
+                    if (Mes == 9)
+                    {
+                        MesFecha = "Septiembre";
+                        if (Dia > 0 && Dia < 30)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 30 && Dia <= 31)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Octubre";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+
+                case 10:
+                    if (Mes == 10)
+                    {
+                        MesFecha = "Octubre";
+                        if (Dia > 0 && Dia < 31)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 31 && Dia <= 32)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Noviembre";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+
+                case 11:
+                    if (Mes == 11)
+                    {
+                        MesFecha = "Noviembre";
+                        if (Dia > 0 && Dia < 30)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 30 && Dia <= 31)
+                        {
+                            Dia = 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            MesFecha = "Diciembre";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+
+                        }
+                    }
+                    break;
+
+                case 12:
+                    if (Mes == 12)
+                    {
+                        MesFecha = "Diciembre";
+                        if (Dia > 0 && Dia < 31)
+                        {
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else if (Dia >= 31 && Dia <= 32)
+                        {
+                            Dia = 1;
+                            Anio = Anio + 1;
+                            DiaFecha = DiaFecha = Convert.ToString(Dia);
+                            AnioFecha = AnioFecha = Convert.ToString(Anio);
+                            MesFecha = "Enero";
+                            MessageBox.Show(DiaFecha + " de " + MesFecha + " de " + AnioFecha);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Se introdujo un número de día que no corresponde al mes");
+                        }
+                    }
+                    break;
+                default: MessageBox.Show("Se introdujo un mes que no corresponde");
+                    break;
+                    //Agregar Default
+
+            }
+
+
+        }
+        
+    }
+
+}
